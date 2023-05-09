507ff7e09298b6157b41b1e8641715dc7b53b0a7
mysteryman04 <112248124+mysteryman04@users.noreply.github.com>
2023-05-10 01:14:48 +0600

    Testing current accomplishment

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        MyHashTable<String, Integer> hashTable = new MyHashTable<>();
+        hashTable.put("iphone", 3);
+        hashTable.put("samsung", 2);
+        hashTable.put("oneplus", 5);
+        System.out.println(hashTable.get("samsung"));
+        System.out.println(hashTable.get("oneplus"));
+        System.out.println(hashTable.get("iphone"));
 
     }
 }
