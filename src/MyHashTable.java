c8119354ce678a67db033fe6d8926d78b5d4ac18
mysteryman04 <112248124+mysteryman04@users.noreply.github.com>
2023-05-09 23:22:03 +0600

    put method inserts new key value pair into hash table by computing the hash code of the key, finding the head node. Also, iterating through the list to update the value: If the key already exists or inserting a new node and if the key does not exist.

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -31,7 +31,21 @@ public class MyHashTable<K, V> {
     private int hash (K key) {
         return (key.hashCode() & 0x7fffffff) % M;
     }
-    public void put (K key, V value) {}
+    public void put (K key, V value) {
+        int index = hash(key);
+        HashNode<K, V> node = chainArray[index];
+        while (node != null) {
+            if (node.key.equals(key)) {
+                node.value = value;
+                return;
+            }
+            node = node.next;
+        }
+        node = new HashNode<K, V>(key, value);
+        node.next = chainArray[index];
+        chainArray[index] = node;
+        size++;
+    }
     public V get (K key) {}
     public V remove (K key) {}
     public boolean contains (V value) {}
