49a3b326efee99fd04238ab710044eba3a6df2c9
mysteryman04 <112248124+mysteryman04@users.noreply.github.com>
2023-05-09 22:49:01 +0600

    initializing the chainArray instance variable with a new array of HashNode objects of size M, and setting the size instance variable to 0

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -1,5 +1,18 @@
 public class MyHashTable<K, V> {
     private class HashNode<K, V> {
+        private K key;
+        private V value;
+        private HashNode<K, V> next;
+
+        public HashNode(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "{" + key +" "+ value + "}";
+        }
     }
     private HashNode<K, V> [] chainArray; // or Obiect
     private int M = 11; // derault number o† chains
@@ -7,10 +20,14 @@ public class MyHashTable<K, V> {
 
     public MyHashTable() {}
 
-    public MyHashTable(int M) {}
-    private int hash (K key) {}
+    public MyHashTable(int M) {
+        this.chainArray = new HashNode[M];
+        this.size = 0;
+    }
+    private int hash (K key) { }
     public void put (K key, V value) {}
-    public V get (K key) {} public V remove (K key) {}
+    public V get (K key) {}
+    public V remove (K key) {}
     public boolean contains (V value) {}
     public K getKey (V value) {}
 }
