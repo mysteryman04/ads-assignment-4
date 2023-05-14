2f9d41142ea1cec507b5ac783268a9021fc825c8
mysteryman04 <112248124+mysteryman04@users.noreply.github.com>
2023-05-14 23:49:37 +0600

    MyTestingClass is a test class with private member variables SSN and brand. It has a constructor that initializes these variables using provided values. The class includes a main method, which is currently empty and can be used for testing or program execution.

---
+++ src/MyTestingClass.java
@@ -0,0 +1,14 @@
+
+public class MyTestingClass {
+    private int SSN;
+    private String brand;
+
+    public MyTestingClass(int id, String name) {
+        this.SSN =id;
+        this.brand =name;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
