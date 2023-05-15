d33c5f53d6a533487ec29dd5b4d1ff0d7823f69b
mysteryman04 <112248124+mysteryman04@users.noreply.github.com>
2023-05-15 06:28:54 +0600

    The hashCode calculates a hash code by combining the hash codes of the SSN and brand member variables using a multiplication and addition operation.

--- src/MyTestingClass.java
+++ src/MyTestingClass.java
@@ -8,6 +8,29 @@ public class MyTestingClass {
         this.brand =name;
     }
 
+    public int hashCode() {
+        int result=17;
+        result=31*result+ SSN;
+        result=31*result+ brand.hashCode();
+        return result;
+    }
+
+    public int getSSN() {
+        return SSN;
+    }
+
+    public void setId() {
+        this.SSN = SSN;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void  setBrand() {
+        this.brand = brand;
+    }
+
     public static void main(String[] args) {
 
     }
